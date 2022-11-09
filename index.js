cbbd3baf32ec2be6a88b3cec40138b2bbc178614
mkrasnansk <miso.krasnansky@gmail.com>
2022-11-09 22:55:18 +0100

    first aws init

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const app = express();
+const port = process.env.PORT || 4000;
+const { response } = require("express");
+const multiparty = require("multiparty");
+require("dotenv").config();
+
+app.use(cors("*"));
+app.use(express.urlencoded());
+app.use(bodyParser.json());
+
+app.post("/mail", function (req, res) {
+   console.log(process.env.OAUTH_REFRESH_TOKEN);
+
+   let data = req.body;
+   if (!data.url) {
+      res.status(500).send({ website: data.url, error: "Missing url" });
+      return;
+   } else {
+      res.status(200).send({ website: data.url, websiteStatus: response.status });
+      if (response.status !== 200) {
+         sendMail(data, response.status);
+      }
+   }
+});
+
+const sendMail = (data, status) => {
+   let transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+         type: "OAuth2",
+         user: process.env.MAIL_USERNAME,
+         pass: process.env.MAIL_PASSWORD,
+         clientId: process.env.OAUTH_CLIENTID,
+         clientSecret: process.env.OAUTH_CLIENT_SECRET,
+         refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+      },
+   });
+
+   let mailOptions = {
+      from: data.name,
+      to: "miso.krasnansky@gmail.com",
+      subject: "Nodemailer Project",
+      text: "Hi from your nodemailer project" + data.url,
+   };
+
+   transporter.sendMail(mailOptions, function (err, data) {
+      if (err) {
+         console.log("Error " + err);
+      } else {
+         console.log("Email sent successfully");
+      }
+   });
+};
+
+app.listen(port, () => {
+   console.log(`nodemailerProject is listening at http://localhost:${port}`);
+});
