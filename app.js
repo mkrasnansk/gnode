dd08d46cfb86a04b47785bae22ea9695f7aa2423
mkrasnansk <miso.krasnansky@gmail.com>
2022-11-11 23:29:56 +0100

    asd

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(cors(corsOptions));
 app.use((req, res, next) => {
    res.setHeader("Access-Control-Allow-Origin", "*");
    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
-   res.setHeader("Access-Control-Allow-Methods", "Content-Type", "Authorization");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
    next();
 });
 
